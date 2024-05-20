05bb8f7e907099cd4752f0f7be59a18e28a568c3
Chingiz_Nurzhanov <90819855+NurzhanovChingiz@users.noreply.github.com>
2024-05-20 16:23:30 +0500

    Create sum_activation.py

---
+++ experimentation/activator/sum_activation.py
@@ -0,0 +1,15 @@
+class Sum_Activation(nn.Module):
+    def __init__(self, activations, init_weights='uniform'):
+        super().__init__()
+        
+        self.activations = nn.ModuleList(activations)
+        self.weights = nn.Parameter(torch.randn(len(activations)))
+        nn.init.uniform_(self.weights)
+
+    def forward(self, x):
+        # Compute outputs of all activation functions and stack them along a new dimension
+        activations_output = torch.stack([activation(x) for activation in self.activations], dim=0)
+        
+        # Use broadcasting to multiply weights and sum up the activations
+        out = torch.einsum('i,ij...->j...', self.weights, activations_output)
+        return out
