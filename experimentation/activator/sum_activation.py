52b57b3463d112a39414bc1d8b15db22c50f0e91
Chingiz_Nurzhanov <90819855+NurzhanovChingiz@users.noreply.github.com>
2024-07-03 16:05:40 +0500

    Update sum_activation.py
    init weights ones

--- experimentation/activator/sum_activation.py
+++ experimentation/activator/sum_activation.py
@@ -1,11 +1,14 @@
 class Sum_Activation(nn.Module):
-    def __init__(self, activations):
+    def __init__(self, activations, init_weights='ones'):
         super().__init__()
         
         self.activations = nn.ModuleList(activations)
         self.weights = nn.Parameter(torch.randn(len(activations)))
-        nn.init.uniform_(self.weights)
-
+        if init_weights == 'ones':
+            nn.init.ones_(self.weights)
+        if init_weights == 'uniform':
+            nn.init.uniform_(self.weights)
+        
     def forward(self, x):
         # Compute outputs of all activation functions and stack them along a new dimension
         activations_output = torch.stack([activation(x) for activation in self.activations], dim=0)
